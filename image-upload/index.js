4430654798681068e61d74661d3230dadc8e0fe5
tohidkhann508 <tohidkhann508@gmail.com>
2026-06-23 08:32:14 +0530

    image upload folder

--- image-upload/index.js
+++ image-upload/index.js
@@ -1,5 +1,5 @@
-const path = require("path");
 const express = require("express");
+const path = require("path");
 const multer = require("multer");
 
 const app = express();
@@ -7,10 +7,10 @@ const PORT = 8000;
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    return cb(null, "./uploads");
+    cb(null, "./uploads");
   },
   filename: function (req, file, cb) {
-    return cb(null, `${Date.now()}-${file.originalname}`);
+    cb(null, `${Date.now()}-${file.originalname}`);
   },
 });
 
@@ -19,21 +19,17 @@ const upload = multer({ storage });
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
 
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded());
 
 app.get("/", (req, res) => {
   return res.render("homepage");
 });
 
-app.post(
-  "/upload",
-  upload.fields([{ name: "profileImage" }, { name: "coverImage" }]),
-  (req, res) => {
-    console.log(req.body);
-    console.log(req.file);
+app.post("/upload", upload.single("profileImage"), (req, res) => {
+  console.log(req.body);
+  console.log(req.file);
 
-    return res.redirect("/");
-  },
-);
+  return res.redirect("/");
+});
 
-app.listen(PORT, () => console.log(`Server Started on PORT: ${PORT}`));
+app.listen(PORT, () => console.log(`Server Started at PORT:${PORT}`));
